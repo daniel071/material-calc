77626a2bc2707fb040b1b4234f44b6b8642beea3
Daniel Pavela <daniel.pavela@gmail.com>
2020-06-18 17:00:02 +1000

    Added support for different operations

--- app/src/main/java/com/daniel/calculator/MainActivity.kt
+++ app/src/main/java/com/daniel/calculator/MainActivity.kt
@@ -2,8 +2,10 @@ package com.daniel.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
 
 
@@ -17,6 +19,11 @@ class MainActivity : AppCompatActivity() {
 
     // Called when the user taps the Calculate! button
     fun calculateYaBoomer(view: View) {
+        // If you ever need to log, use this: Log.d("thisWasSelected", currentOperation);
+
+        val spinner: Spinner = findViewById(R.id.operationSpinner)
+        val currentOperation = spinner.getSelectedItem().toString();
+
         // Do something in response to button
         val textView = findViewById(R.id.theResult) as TextView
 
@@ -26,7 +33,18 @@ class MainActivity : AppCompatActivity() {
         val textTwo = findViewById<View>(R.id.entryTwo) as EditText
         val secondInput = textTwo.text.toString().toFloat()
 
-        val total = firstInput + secondInput
+        var total = 0f
+
+        if (currentOperation == "Add") {
+            total = firstInput + secondInput
+        } else if (currentOperation == "Subtract") {
+            total = firstInput - secondInput
+        } else if (currentOperation == "Multiply") {
+            total = firstInput * secondInput
+        } else if (currentOperation == "Divide") {
+            total = firstInput / secondInput
+        }
+
 
         textView.setText(total.toString())
     }
